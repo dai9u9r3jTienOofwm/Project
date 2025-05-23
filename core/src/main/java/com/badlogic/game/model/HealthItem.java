2c20d9bdd00781c62fb4b19ff8ba4aeda5fe4c81
Hassoku26 <sonvungoc26@gmail.com>
2025-05-23 17:04:25 +0700

    Add files via upload

--- core/src/main/java/com/badlogic/game/model/HealthItem.java
+++ core/src/main/java/com/badlogic/game/model/HealthItem.java
@@ -1,5 +1,58 @@
 package com.badlogic.game.model;
 
-public class HealthItem {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+public class HealthItem extends BaseEntity{
+    private float speed = 200f;
+    public int healAmount;
+
+    public HealthItem(float x, float y) {
+        super(new Texture("HealItem.png"), x, y);
+        this.healAmount = 1;
+
+    }
+
+    public HealthItem(float x, float y, int healAmount) {
+        super(new Texture("HealItem.png"), x, y);
+        this.healAmount = healAmount;
+
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setHealAmount(int healAmount) {
+        this.healAmount = healAmount;
+    }
+
+    public int getHealAmount() {
+        return healAmount;
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        y -= speed * deltaTime;
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, x, y);
+    }
+
+    @Override
+    public boolean isAlive() {
+        return alive;
+    }
+
+    @Override
+    public void setAlive(boolean alive) {
+        super.setAlive(alive);
+    }
 }
+
